c54bae51bcc6cda711bb0342b3d8156e40cf9429
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-07-06 00:32:20 +0900

    【新機能】ボスのプレビューUIを作成

---
+++ Assets/Scenes/Scripts/UIBossConfigView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBossConfigView : MonoBehaviour
+{
+    [SerializeField] private Text _bossNameUi;
+    [SerializeField] private Text _descriptionUi;
+    [SerializeField] private Image _bossIconUi;
+
+    public void Apply(string name, string description, Sprite img)
+    {
+        _bossNameUi.text = name.ToUpper();
+        _descriptionUi.text = description;
+        _bossIconUi.sprite = img;
+    }
+}
---
+++ Assets/Scenes/Scripts/UIBossPreviewWarningWindow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBossPreviewWarningWindow : MonoBehaviour
+{
+    [SerializeField] private UIBossConfigView _bossConfigView;
+
+    private void Start()
+    {
+        _bossConfigView.Apply("test","test",null);
+    }
+}
