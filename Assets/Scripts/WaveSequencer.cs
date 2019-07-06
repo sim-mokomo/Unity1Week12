45377af0d1cf7ea8e05fb8fd8c222a5420602746
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-07-06 11:53:23 +0900

    【新機能】Waveを操作する役割のクラスを作成。Waveを変えたい時に頼る

---
+++ Assets/Scripts/WaveSequencer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSequencer : MonoBehaviour
+{
+    [SerializeField] private List<StageWave> _stageWaves;
+    private int _currentWaveIndex = 0;
+
+    public void Initialize()
+    {
+        _currentWaveIndex = 0;
+    }
+    
+    public StageWave FetchCurrentWave()
+    {
+        return _stageWaves[_currentWaveIndex];
+    }
+
+    public void MoveNextWave()
+    {
+        _currentWaveIndex = (_currentWaveIndex + 1) % _stageWaves.Count;
+    }
+}
