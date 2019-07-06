24d7585ee8a43bbfe8510a1beff1aec13a46b1cd
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-07-06 17:52:46 +0900

    【調整】デバッグしやすいように起動時のシーンを選べるようにした。

--- Assets/Scripts/GameSequencer.cs
+++ Assets/Scripts/GameSequencer.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public enum GameSequence
 {
+    None,
     BossPreview,
+    BossBattle,
     Shop,
 }
 
@@ -12,11 +14,15 @@ public class GameSequencer : MonoBehaviour
 {
     [SerializeField] private UIBossPreviewWarningWindow _uiBossPreviewWarningWindow;
     [SerializeField] private UIShopWindow _uiShopWindow;
+    [SerializeField] private UIBossBattleWindow _uiBossBattleWindow;
     [SerializeField] private WaveSequencer _waveSequencer;
+
+    [Header("Debug")]
+    [SerializeField] private GameSequence _initDebugSequence;
 
     private void Start()
     {
-        UpdateUIActivate(GameSequence.BossPreview);
+        UpdateUIActivate( _initDebugSequence == GameSequence.None ? GameSequence.BossPreview : _initDebugSequence );
         _uiBossPreviewWarningWindow.ApplyBossConfig(_waveSequencer.FetchCurrentWave().BossConfig);
 
         _uiBossPreviewWarningWindow.OnNextSequence += () =>
@@ -38,10 +44,17 @@ public class GameSequencer : MonoBehaviour
         {
                 case GameSequence.BossPreview :
                     _uiBossPreviewWarningWindow.gameObject.SetActive(true);
+                    _uiBossBattleWindow.gameObject.SetActive(false);
+                    _uiShopWindow.gameObject.SetActive(false);
+                    break;
+                case GameSequence.BossBattle:
+                    _uiBossPreviewWarningWindow.gameObject.SetActive(false);
+                    _uiBossBattleWindow.gameObject.SetActive(true);
                     _uiShopWindow.gameObject.SetActive(false);
                     break;
                 case GameSequence.Shop:
                     _uiBossPreviewWarningWindow.gameObject.SetActive(false);
+                    _uiBossBattleWindow.gameObject.SetActive(false);
                     _uiShopWindow.gameObject.SetActive(true);
                     break;
         }
