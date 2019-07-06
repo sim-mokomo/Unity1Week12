1754620269f25c695ddfe284ea29da2d72e24e22
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-07-06 17:51:17 +0900

    【新機能】拾えるアイテムを表示を試せるようにした

---
+++ Assets/Scripts/UIBossBattleWindow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBossBattleWindow : MonoBehaviour
+{
+    [SerializeField] private PickupedCellScrollView _pickupedCellScrollView;
+
+    private void OnEnable()
+    {
+        var player = FindObjectOfType<Player>();
+        player.PickupedItems.ForEach(p =>
+        {
+            
+            var cell = PickupedItemCellFactory.Instance.CreateCell(p, Vector3.zero, Quaternion.identity,Vector3.one);
+            _pickupedCellScrollView.Add(cell);
+        });
+    }
+}
