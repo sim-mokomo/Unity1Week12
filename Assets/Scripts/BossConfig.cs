d946a2c4aae621b1efb267c8fc4cb132f1603153
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-07-06 11:50:02 +0900

    【新機能】ボスの設定値クラスを作成

---
+++ Assets/Scripts/BossConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BossConfig : ScriptableObject
+{
+    [SerializeField] private string _name;
+    [SerializeField]private string _description;
+    [SerializeField]private Sprite _icon;
+    
+    public string Name => _name;
+    public string Description => _description;
+    public Sprite Icon => _icon;
+}
