ec3721cd3da2cf77e5988d8c9eb614ca677689d1
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-07-06 12:39:00 +0900

    【新機能】ショップウィンドウ定義、タップすると次の画面に移動する。

---
+++ Assets/Scripts/UIShopWindow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIShopWindow : MonoBehaviour
+{
+    public event Action OnNextSequence;
+    
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+            OnNextSequence?.Invoke();
+    }
+}
