76e05099850de566289c1c2f46eba4180812808d
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2019-07-06 17:47:29 +0900

    【新機能】拾えるアイテム表示用セルクラス作成

---
+++ Assets/Scripts/EquipmentCell.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EquipmentCell : MonoBehaviour,IPickupedItemCell
+{
+    [SerializeField] private Text _name;
+    [SerializeField] private Text _attack;
+    [SerializeField] private Text _defence;
+    [SerializeField] private Text _durable;
+    
+    public void Setup(IPickupedItem pickupedItem)
+    {
+        var equipment = pickupedItem as Equipment;
+        _name.text = equipment.Name;
+        _attack.text = $"攻:{equipment.Attack}";
+        _defence.text = $"防:{equipment.Defence}";
+        _durable.text = $"耐久:{equipment.Durable}";
+    }
+}
---
+++ Assets/Scripts/IPickupedItemCell.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPickupedItemCell
+{
+    void Setup(IPickupedItem pickupedItem);
+}
